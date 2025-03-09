648282f0d308ab2fbf7b6f41983e3bf180392023
yvon voyelle <Voyelleyvon1@gmail.com>
2025-03-09 23:57:56 +0000

    completed lab fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -2,19 +2,38 @@
 public class Fibonacci {
     /**
      * Return the nth number of fibonacci sequence.
-     * The fibonacci sequence is calculated by adding the previous two numbers in the sequence to obtain the next
-     * number. The first two numbers in the whole fibonacci sequence are always 0 and 1.
+     * The fibonacci sequence is calculated by adding the previous two numbers in
+     * the sequence to obtain the next
+     * number. The first two numbers in the whole fibonacci sequence are always 0
+     * and 1.
      * For instance, the sequence starts as:
      * 0 1 1 2 3 5 8 13 21
-     * where the 4th number of the sequence would be 3, the 5th number would be 5, the 6th number would be 8, etc.
+     * where the 4th number of the sequence would be 3, the 5th number would be 5,
+     * the 6th number would be 8, etc.
      *
-     * You could either use a for loop to keep track of the current number of the fibonacci sequence as well as the two
-     * numbers before it, or you could look up a recursive solution to experiment with recursion for the first time.
+     * You could either use a for loop to keep track of the current number of the
+     * fibonacci sequence as well as the two
+     * numbers before it, or you could look up a recursive solution to experiment
+     * with recursion for the first time.
      *
      * @param n an iteration of the fibonacci sequence.
      * @return the nth number of fibonacci sequence.
      */
-    public int fib(int n){
-        return 0;
+    public int fib(int n) {
+        int rs_1 = 0;
+        int rs_2=0;
+
+        for (int i = 0; i < n; i--) {
+
+
+            rs_1 = (n - 1);
+            rs_2 = rs_1 -2;
+            if (n==1){
+                return n;
+            }
+
+
+        }
+        return rs_1+rs_2;
     }
 }
